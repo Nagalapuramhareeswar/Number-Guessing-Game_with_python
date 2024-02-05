69af817f7e54bbeacacd729af5131e58d835c8e5
NAGALAPURAM HAREESWAR <135661045+Nagalapuramhareeswar@users.noreply.github.com>
2024-02-06 01:27:11 +0530

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,8 +2,8 @@ import random
 random_number=random.randrange(0, 10)
 print(random_number)
 print("please ensure you have only three chances to guess the number")
-for i in range(3):
-    k=int(input("enter the number "+str(i)+" chance "+" : "))
+for i in range(1,4):
+    k=int(input("enter the number "+" "+str(i)+" : "))
     if(k>int(random_number)):
         print("greater number")
     elif(k<int(random_number)):
@@ -11,3 +11,5 @@ for i in range(3):
     elif(k==int(random_number)):
         print("hoorahhhh....!!!!")
         break
+if(k!=int(random_number)):
+    print("you lost the game.......... ;_; ")
